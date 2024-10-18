d5673b293aac1d6a67cd10114477d27f2dc33a7d
thanhtina8 <thanhtina8@gmail.com>
2024-10-18 21:51:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home Udacity</h3>"
+    html = "<h3>Sklearn Prediction Home Udacity Project 2</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
